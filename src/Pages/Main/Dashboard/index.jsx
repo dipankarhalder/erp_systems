43dbfb640f656f8e11855510bded9052d4eac4b3
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-08-08 05:06:11 +0530

    add

--- src/Pages/Main/Dashboard/index.jsx
+++ src/Pages/Main/Dashboard/index.jsx
@@ -1,7 +1,28 @@
 import { TopBar } from "../../../Components/Main/TopBar";
 import { locationInfo } from "../../../Constant/index";
-import { UpGraph, DownGraph } from "../../../Shared/Icons";
+import {
+  Companies,
+  Subscribers,
+  Revenue,
+  Income,
+  NetProfit,
+  Sale,
+  Loans,
+  Darrow,
+  Danalysis,
+  Uanalysis,
+  Settings,
+  Teams,
+  Profile,
+  Members,
+  Rarrow,
+  Plans,
+  Stocks,
+} from "../../../Shared/Icons";
 import { AppMainLayoutCover, AppDeashboardSection } from "../style";
+import { RevenueChart } from "../../../Components/Main/Dashboard/RevenueChart";
+import { ExpensesGrossProfitChart } from "../../../Components/Main/Dashboard/ExpensesGrossProfitChart";
+import { AccountsLineChart } from "../../../Components/Main/Dashboard/AccountsLineChart";
 
 const pagePaths = [{ label: "..Apps", path: "/" }];
 
@@ -10,69 +31,210 @@ export const DashboardPage = () => {
     <AppMainLayoutCover>
       <TopBar pageName="Dashboard" items={pagePaths} location={locationInfo} />
       <AppDeashboardSection>
-        <div className="app_revenue_sec">
-          <h1>Report Overview</h1>
-          <div className="app_main_revenue">
-            <div className="app_item_rev">
-              <p>Revenue</p>
-              <div className="app_main_rev_items">
-                <h2>
-                  Rs. 528,976<span>.82 /-</span>
-                </h2>
-                <div className="app_rev_items">
-                  <div className="app_rev_list_item">
-                    <p>56%</p>
-                  </div>
-                  <div className="app_rev_list_item">
-                    <p>Rs. 48,586.00/-</p>
-                  </div>
-                </div>
+        <div className="app_heading_section">
+          <h2>
+            Hello, <span>Himadri Chakraborti</span>
+          </h2>
+          <p>
+            Wishing you a fantastic day - I hope everything is going smoothly
+            and bringing you joy.
+          </p>
+        </div>
+        <div className="app_list_overview_items">
+          <div className="app_list_info">
+            <div className="app_list_row">
+              <div className="app_list_heading">
+                <h3>Total Revenue</h3>
+                <p>Rs. 528976/-</p>
               </div>
-              <div className="app_rev_items_new">
-                <p>
-                  Prev month - <span>Rs. 490,976.34/-</span>
-                </p>
-                <p>
-                  Sep 1, 2024 - <span>July 31, 2025</span>
-                </p>
+              <div className="app_list_icons revenue">
+                <Revenue />
               </div>
             </div>
-            <div className="app_side_revenue">
-              <ul>
-                <li>
-                  <p>Today's Revenue</p>
-                  <span>Rs. 53.200/-</span>
-                  <div className="rev_graph_view">
-                    <div className="inside_rev green_view">
-                      <UpGraph />
-                      <p>23%</p>
-                    </div>
-                  </div>
+            <div className="app_graph_sec">
+              <ul className="upitem">
+                <li className="app_graph_itm">
+                  <Uanalysis />
+                  <p>6.9%</p>
                 </li>
-                <li>
-                  <p>Today's Orders</p>
-                  <span>346 items</span>
-                  <div className="rev_graph_view">
-                    <div className="inside_rev green_view">
-                      <UpGraph />
-                      <p>23%</p>
-                    </div>
-                  </div>
+                <li className="app_week">
+                  <p>This week</p>
                 </li>
-                <li>
-                  <p>Vendor Refunds</p>
-                  <span>Rs. 21.790/-</span>
-                  <div className="rev_graph_view">
-                    <div className="inside_rev red_view">
-                      <DownGraph />
-                      <p>23%</p>
-                    </div>
-                  </div>
+                <li className="app_today">
+                  <p>Today</p>
+                  <Darrow />
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="app_list_info">
+            <div className="app_list_row">
+              <div className="app_list_heading">
+                <h3>Total Income</h3>
+                <p>Rs. 128900/-</p>
+              </div>
+              <div className="app_list_icons income">
+                <Income />
+              </div>
+            </div>
+            <div className="app_graph_sec">
+              <ul className="downitem">
+                <li className="app_graph_itm">
+                  <Danalysis />
+                  <p>4.2%</p>
+                </li>
+                <li className="app_week">
+                  <p>This week</p>
+                </li>
+                <li className="app_today">
+                  <p>Today</p>
+                  <Darrow />
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="app_list_info">
+            <div className="app_list_row">
+              <div className="app_list_heading">
+                <h3>Net Profit</h3>
+                <p>Rs. 108006/-</p>
+              </div>
+              <div className="app_list_icons netprofit">
+                <NetProfit />
+              </div>
+            </div>
+            <div className="app_graph_sec">
+              <ul className="upitem">
+                <li className="app_graph_itm">
+                  <Uanalysis />
+                  <p>6.9%</p>
+                </li>
+                <li className="app_week">
+                  <p>This week</p>
+                </li>
+                <li className="app_today">
+                  <p>Today</p>
+                  <Darrow />
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="app_list_info">
+            <div className="app_list_row">
+              <div className="app_list_heading">
+                <h3>Total Sales</h3>
+                <p>Rs. 23730/-</p>
+              </div>
+              <div className="app_list_icons sale">
+                <Sale />
+              </div>
+            </div>
+            <div className="app_graph_sec">
+              <ul className="upitem">
+                <li className="app_graph_itm">
+                  <Uanalysis />
+                  <p>6.9%</p>
+                </li>
+                <li className="app_week">
+                  <p>This week</p>
+                </li>
+                <li className="app_today">
+                  <p>Today</p>
+                  <Darrow />
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="app_list_info">
+            <div className="app_list_row">
+              <div className="app_list_heading">
+                <h3>Loan Amounts</h3>
+                <p>Rs. 187300/-</p>
+              </div>
+              <div className="app_list_icons loans">
+                <Loans />
+              </div>
+            </div>
+            <div className="app_graph_sec">
+              <ul className="downitem">
+                <li className="app_graph_itm">
+                  <Danalysis />
+                  <p>6.9%</p>
+                </li>
+                <li className="app_week">
+                  <p>This week</p>
+                </li>
+                <li className="app_today">
+                  <p>Today</p>
+                  <Darrow />
                 </li>
               </ul>
             </div>
           </div>
         </div>
+        <div className="app_add_new_items">
+          <ul>
+            <li>
+              <div className="front_item">
+                <Companies />
+                <p>Warehouse</p>
+                <Rarrow />
+              </div>
+            </li>
+            <li>
+              <div className="front_item">
+                <Subscribers />
+                <p>Products</p>
+                <Rarrow />
+              </div>
+            </li>
+            <li>
+              <div className="front_item">
+                <Plans />
+                <p>Order</p>
+                <Rarrow />
+              </div>
+            </li>
+            <li>
+              <div className="front_item">
+                <Settings />
+                <p>Services</p>
+                <Rarrow />
+              </div>
+            </li>
+            <li>
+              <div className="front_item">
+                <Teams />
+                <p>Customers</p>
+                <Rarrow />
+              </div>
+            </li>
+            <li>
+              <div className="front_item">
+                <Profile />
+                <p>Employee</p>
+                <Rarrow />
+              </div>
+            </li>
+            <li>
+              <div className="front_item">
+                <Members />
+                <p>Vendors</p>
+                <Rarrow />
+              </div>
+            </li>
+            <li>
+              <div className="front_item">
+                <Stocks />
+                <p>Materials</p>
+                <Rarrow />
+              </div>
+            </li>
+          </ul>
+        </div>
+        <RevenueChart />
+        <ExpensesGrossProfitChart />
+        <AccountsLineChart />
       </AppDeashboardSection>
     </AppMainLayoutCover>
   );
