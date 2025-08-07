74281e18e222fbfe10a6d37a67e7adf8fc7d4a05
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-08-08 05:17:10 +0530

    update folder

--- src/Pages/Main/Events/index.jsx
+++ src/Pages/Main/Events/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { TopBar } from "../../../Components/Main/TopBar";
-import { locationInfo } from "../../../Constant/index";
+import { locationInfo } from "../../../Sconstant";
 import { TableInfo } from "../../../Shared/Table";
 import { AppMainLayoutCover, AppTableDataInformation } from "../style";
 import eventData from "../../../data/events.json";
